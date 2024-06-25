1a71f38e366427897aa113ca02b7e95b881d0e50
MI7 <muhammadidris204@gmail.com>
2024-06-25 10:13:37 +0400

    my courwork 3, initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>{{ sitename }}</h1>
+      <button @click="showCheckout">{{ this.cart.length }}Checkout</button>
+    </header>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "app",
+    data() {
+      return {
+        sitename: "Mr-Mohs School",
+        cart: []
+      }
+    },
+    methods: {showCheckout() {}}
+  }
+</script>
