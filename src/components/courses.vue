5b23b7d06f28e92e106bc37fe180622accd9fa5d
MI7 <muhammadidris204@gmail.com>
2024-06-25 10:22:29 +0400

    template

---
+++ src/components/courses.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div v-for="course in courses" :key="course.id">
+            <h2>{{ course.title }}</h2>
+            <figure>
+                <img v-bind:src="course.img"/>
+            </figure>
+            <p v-html="course.description"></p>
+            <p>Price: {{ course.price }}</p>
+            <p>Available Stock: {{ course.availableInventory }}</p>
+            <button @click="add(product)">Add to cart</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: "courseList",
+        data(){
+            
+        }
+    }
+</script>
